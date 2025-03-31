c5300f0b5e15e229b79714c07c95b97a94e4781f
unigurii <2836865297@qq.com>
2025-03-31 18:32:46 +0800

    swiftest ser_data_read

--- ser_data_read.py
+++ ser_data_read.py
@@ -8,7 +8,7 @@ import threading
 import queue
 import time
 from queue import LifoQueue #注意FIFO造成的延迟问题
-import copy #注意深浅拷贝的问题
+#import copy #注意深浅拷贝的问题
 
 
 port = 'COM25'  # 串口号，根据实际情况修改
@@ -56,14 +56,7 @@ ax.quiver(0, 0, 0, 0, 0, 100, color='b', arrow_length_ratio=0.1)  # 正Z轴，�
 ax.quiver(0, 0, 0, 0, 0, -100, color='b', arrow_length_ratio=0)   # 负Z轴，无箭头
 ax.text(0, 0, 110, 'Z', color='b')  # 标注Z轴
 
-# 初始化数据点
-# x, y, z = 0, 0, 0
-# scatter = ax.scatter(x, y, z, color='r')
-# line, = ax.plot([0, x], [0, y], [0, z], color='k')  # 连接原点和目标点的线
 
-# x1, y1, z1 = 0, 0, 0
-# scatter1 = ax.scatter(x1, y1, z1, color='b')
-# line1, = ax.plot([0, x1], [0, y1], [0, z1], color='k')  # 连接原点和目标点的线
 
 # 初始化数据点
 point_dict = {'coordinate':[0,0,0],
@@ -79,14 +72,6 @@ point_dict1 = {'coordinate':[0,0,0],
 points_dict_set = {tag_id1:point_dict,
                    tag_id2:point_dict1} 
 
-# points_dict_set = {tag_id1: copy.deepcopy(point_dict), 
-#                    tag_id2: copy.deepcopy(point_dict)} #注意深浅拷贝的问题
-
-
-
-# 初始化文本显示
-# text = ax.text2D(0.05, 0.95, "", transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
-# text1 = ax.text2D(0.05, 0.50, "", transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
 
 # 数据队列
 data_queue = queue.LifoQueue()
@@ -100,28 +85,28 @@ def process_data(data):
     if len(parts) < 9:
         return 0,0,0,0,0,0,0,0,0
     tag_id = parts[0].split(':')[1]  # 提取Tag ID
-    if tag_id == tag_id_set[tag_flag]:
-        print('the tag_id is:',tag_id)
-        print('the tag_id_set[tag_flag] is:',tag_id_set[tag_flag])
-        if tag_flag != tag_num-1:
-            tag_flag = tag_flag + 1
-        else:tag_flag = 0
-        rssi = float(parts[1])  # RSSI
-        azimuth = float(parts[2])  # 方位角
-        elevation = float(parts[3])  # 仰角
-        timestamp = parts[-1].strip('"')  # 时间戳
+    # if tag_id == tag_id_set[tag_flag]:
+    #     print('the tag_id is:',tag_id)
+    #     print('the tag_id_set[tag_flag] is:',tag_id_set[tag_flag])
+    #     if tag_flag != tag_num-1:
+    #         tag_flag = tag_flag + 1
+    #     else:tag_flag = 0
+    rssi = float(parts[1])  # RSSI
+    azimuth = float(parts[2])  # 方位角
+    elevation = float(parts[3])  # 仰角
+    timestamp = parts[-1].strip('"')  # 时间戳
 
-        # 计算三维坐标
-        distance = 100 * (1 - rssi / -90)  # 将RSSI映射到距离
-        distance = 100
-        #print('the distance is :',distance)
+    # 计算三维坐标
+    distance = 100 * (1 - rssi / -90)  # 将RSSI映射到距离
+    distance = 100
+    #print('the distance is :',distance)
 
-        z = int(abs(distance * np.cos(np.radians(elevation)) * np.cos(np.radians(azimuth))))
-        y = int(-distance * abs(np.cos(np.radians(elevation))) * np.sin(np.radians(azimuth)))
-        x = int(-distance * np.sin(np.radians(elevation)))
+    z = int(abs(distance * np.cos(np.radians(elevation)) * np.cos(np.radians(azimuth))))
+    y = int(-distance * abs(np.cos(np.radians(elevation))) * np.sin(np.radians(azimuth)))
+    x = int(-distance * np.sin(np.radians(elevation)))
 
-        return x, y, z, rssi, azimuth, elevation, tag_id, timestamp,1
-    else:return 0,0,0,0,0,0,0,0,0
+    return x, y, z, rssi, azimuth, elevation, tag_id, timestamp,1
+    #else:return 0,0,0,0,0,0,0,0,0
 
 
 # 串口读取线程
@@ -144,51 +129,32 @@ def update(frame):
     scatter_set = []
     line_set = []
     text_set = []
-    if not data_queue.empty():
-        x, y, z, rssi, azimuth, elevation, tag_id, timestamp = data_queue.get()
-        #x, y, z, rssi, azimuth, elevation, tag_id, timestamp = 0,0,0,0,0,0,0,0
-        #print('#########',tag_id)
-        #print('$$$$$$$$$$',tag_id_set[tag_flag])
-        # if tag_id == tag_id_set[tag_flag]:
-            # if tag_flag != tag_num-1:
-            #     tag_flag = tag_flag + 1
-            # else:tag_flag = 0
-            
-        # if tag_id == tag_id1:
-        #     # 更新散点图的坐标
-        #     scatter._offsets3d = ([x], [y], [z])
-        #     # 更新连接线
-        #     line.set_data([0, x], [0, y])
-        #     line.set_3d_properties([0, z])
-        #     # 更新文本显示
-        #     text.set_text(f"RSSI: {rssi}\nAzimuth: {azimuth}°\nElevation: {elevation}°\nTag ID: {tag_id}\nTimestamp: {timestamp}")
+    #if not data_queue.empty(): s
+    x, y, z, rssi, azimuth, elevation, tag_id, _ = data_queue.get()
+    while tag_id!=tag_id_set[tag_flag]:
+        x, y, z, rssi, azimuth, elevation, tag_id, _ = data_queue.get()
+    if tag_flag != tag_num-1:
+        tag_flag = tag_flag + 1
+    else:tag_flag = 0
 
-        # if tag_id == tag_id2:
-        #     # 更新散点图的坐标
-        #     scatter1._offsets3d = ([x], [y], [z])
-        #     # 更新连接线
-        #     line1.set_data([0, x], [0, y])
-        #     line1.set_3d_properties([0, z])
-        #     # 更新文本显示
-        #     text1.set_text(f"RSSI: {rssi}\nAzimuth: {azimuth}°\nElevation: {elevation}°\nTag ID: {tag_id}\nTimestamp: {timestamp}")
-        points_dict_set[tag_id]['scatter']._offsets3d = ([x], [y], [z])
-        points_dict_set[tag_id]['line'].set_data([0, x], [0, y])        
-        points_dict_set[tag_id]['line'].set_3d_properties([0, z])
-        points_dict_set[tag_id]['text'].set_text(f"RSSI: {rssi}\nAzimuth: {azimuth}°\nElevation: {elevation}°\nTag ID: {tag_id}\nTimestamp: {timestamp}")
-        #print(tag_id)
-        # print('tag_id1',points_dict_set[tag_id1])
-        # print('tag_id2',points_dict_set[tag_id2])
+    points_dict_set[tag_id]['scatter']._offsets3d = ([x], [y], [z])
+    #points_dict_set[tag_id]['line'].set_data([0, x], [0, y])        
+    #points_dict_set[tag_id]['line'].set_3d_properties([0, z])
+    points_dict_set[tag_id]['text'].set_text(f"RSSI: {rssi}\nAzimuth: {azimuth}°\nElevation: {elevation}°\nTag ID: {tag_id}")
+    #print(tag_id)
+    # print('tag_id1',points_dict_set[tag_id1])
+    # print('tag_id2',points_dict_set[tag_id2])
 
-        for tag_id_flag in tag_id_set:
-            scatter_set.append(points_dict_set[tag_id_flag]['scatter'])
-            line_set.append(points_dict_set[tag_id_flag]['line'])
-            text_set.append(points_dict_set[tag_id_flag]['text'])
-        # print(scatter_set)
-        # print(line_set)
-        # print(text_set)
-        #file.write("处理结果：RSSI: {}, Azimuth: {}°, Elevation: {}°, Tag ID: {}, Timestamp: {}\n".format(rssi, azimuth, elevation, tag_id, timestamp))
-        #file.flush()
-    # return [scatter,scatter1], [line,line1], [text,text1]
+    for tag_id_flag in tag_id_set:
+        scatter_set.append(points_dict_set[tag_id_flag]['scatter'])
+        line_set.append(points_dict_set[tag_id_flag]['line'])
+        text_set.append(points_dict_set[tag_id_flag]['text'])
+    # print(scatter_set)
+    # print(line_set)
+    # print(text_set)
+    #file.write("处理结果：RSSI: {}, Azimuth: {}°, Elevation: {}°, Tag ID: {}, Timestamp: {}\n".format(rssi, azimuth, elevation, tag_id, timestamp))
+    #file.flush()
+# return [scatter,scatter1], [line,line1], [text,text1]
     return scatter_set,line_set,text_set
     #else:return 0
 
